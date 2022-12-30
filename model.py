ea4c7d92f8de97da639c5b3b492c676dfb81f6f7
John Lawrence Abarquez <lawrence.abarquez@gmail.com>
2022-12-31 05:46:46 +0800

    Added Venv for WSL Use development

--- model.py
+++ model.py
@@ -4,3 +4,6 @@ from keras.layers import Dense
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing import image
+from keras.applications.vgg16 import VGG16
+
+model = VGG16()
