583bc4def4e1c8828734ddcfe731b26d9564d48f
John Lawrence Abarquez <lawrence.abarquez@gmail.com>
2023-02-15 12:35:33 +0800

    Revert "Imported numpy package"
    This reverts commit 3f5c269983e8195f2d1f8774fddaab0220dd26fa.

--- model.py
+++ model.py
@@ -4,10 +4,11 @@ from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing import image
 from keras.applications.vgg16 import VGG16
-import numpy as np
 
 model = VGG16()
 
 from keras.utils.vis_utils import plot_model
 print(model.summary())
 plot_model(model, to_file='vgg.png')
+
+from tensorflow import keras
