8571bee7baa555e2e675715425414c3f9c4780c4
John Lawrence Abarquez <lawrence.abarquez@gmail.com>
2023-03-06 21:42:54 +0800

    Added File Pre-processors

---
+++ File_Mover.py
@@ -0,0 +1,17 @@
+from glob import glob
+
+# Path setup.
+path = r"C:\Users\John\Documents\Datasets\Consolidated_Photo_Database"
+dest = r"C:\Users\John\Documents\Datasets\Photo_Database"
+
+# Collect files.
+files = glob(os.path.join(path, '**'), recursive=True)
+
+# Ensure destination path exists.
+if not os.path.isdir(dest):
+    os.makedirs(dest)
+# Move all files.
+for f in files:
+    if os.path.isfile(f):
+        base = os.path.basename(f)
+        shutil.move(f, os.path.join(dest, base))
--- model.py
+++ model.py
@@ -1,14 +1,37 @@
+import tensorflow as tf
+from tensorflow import keras
 from keras.models import Model
 from keras.layers import Dense
 from keras import optimizers
 from keras.preprocessing.image import ImageDataGenerator
 from keras.preprocessing import image
 from keras.applications.vgg16 import VGG16
+import numpy as np
+import matplotlib.pyplot as plt
 
-model = VGG16()
 
-from keras.utils.vis_utils import plot_model
-print(model.summary())
-plot_model(model, to_file='vgg.png')
+# To load a file use the format [/mnt/c/path/to/file]
+data = np.load(r'/mnt/c/Users/John/Documents/Datasets/task7/task7_X_test.npy')
+print(data.shape)
 
-from tensorflow import keras
+vgg = VGG16(include_top = False,
+            weights = 'imagenet', 
+            input_tensor = None, 
+            input_shape = (224,224,3), #shape of npy file data
+            pooling = None,
+            classes = 1000,
+            classifier_activation="softmax") 
+
+# Do not retrain convolutional layers
+for layer in vgg.layers:
+    layer.trainable = False
+
+inputs = keras.Input(shape=(224, 224, 3))
+
+# To show all the data in npy file as images [FOR DEMO PURPOSES ONLY]
+img_array=np.load(r'/mnt/c/Users/John/Documents/Datasets/task7/task7_X_test.npy')
+i = 0
+while i < len(img_array):
+   plt.imshow(img_array[i], cmap='gray')
+   plt.show()
+   i += 1
