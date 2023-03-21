15918ff2c52fe12cab3de93097dbd52a7605539e
John Lawrence Abarquez <lawrence.abarquez@gmail.com>
2023-03-22 00:12:50 +0800

    Created initial working model

--- model.py
+++ model.py
@@ -12,8 +12,13 @@ from PIL import Image
 import os 
 
 # To load a file use the format [/mnt/c/path/to/file]
-data = np.load(r'/mnt/c/Users/John/Documents/Datasets/task7/task7_X_train.npy')
-print(data.shape)
+# data = np.load(r'/mnt/c/Users/John/Documents/Datasets/task7/task7_X_train.npy')
+x_train = np.load(r"/mnt/c/Windows/System32/repos/thesis_raw_data/task5/task5_X_train.npy")
+y_train = np.load(r"/mnt/c/Windows/System32/repos/thesis_raw_data/task5/task5_y_train.npy")
+
+x_test = np.load(r"/mnt/c/Windows/System32/repos/thesis_raw_data/task5/task5_X_test.npy")
+y_test = np.load(r"/mnt/c/Windows/System32/repos/thesis_raw_data/task5/task5_y_test.npy")
+print(x_train.shape)
 
 vgg16_model = VGG16(include_top = False,
             weights = 'imagenet', 
@@ -22,6 +27,9 @@ vgg16_model = VGG16(include_top = False,
             pooling = None,
             classes = 1000,
             classifier_activation="softmax") 
+num_classes = 3
+batch_size = 32
+num_epochs = 10
 
 # Do not retrain convolutional layers
 for layer in vgg16_model.layers:
@@ -41,49 +49,98 @@ model = Model(inputs=vgg16_model.input, outputs=output)
 # Compile the model
 model.compile(optimizer='adam', loss='categorical_crossentropy', metrics=['accuracy'])
 
-# Define data generators for training and validation
-train_datagen = ImageDataGenerator(
-    rescale=1./255,
-    shear_range=0.2,
-    zoom_range=0.2,
-    horizontal_flip=True)
+# Create an instance of ImageDataGenerator for data augmentation
+datagen = ImageDataGenerator(rotation_range=20,
+                             width_shift_range=0.1,
+                             height_shift_range=0.1,
+                             shear_range=0.2,
+                             zoom_range=0.2,
+                             horizontal_flip=True)
 
-test_datagen = ImageDataGenerator(rescale=1./255)
+# Fit the ImageDataGenerator on the training data
+datagen.fit(x_train)
 
-train_generator = train_datagen.flow_from_directory(
-    train_dir, #training data directory
-    target_size=input_shape[:2],
-    batch_size=batch_size,
-    class_mode='categorical')
+# Create generators for the training and validation data
+train_generator = datagen.flow(x_train,
+                               y_train, 
+                               batch_size=batch_size,
+                               shuffle=True
+                               )
+                               
+test_generator = datagen.flow(x_test, 
+                              y_test, 
+                              batch_size=batch_size,
+                              shuffle=False)
 
-validation_generator = test_datagen.flow_from_directory(
-    validation_dir, #validation data directory
-    target_size=input_shape[:2],
-    batch_size=batch_size,
-    class_mode='categorical')
+# Use the generators to train the model
+history = model.fit(train_generator,
+          epochs=10,
+          steps_per_epoch=len(train_generator) // batch_size,
+          validation_data=test_generator)
 
-# Train the model
-history = model.fit_generator(
-    train_generator,
-    steps_per_epoch=train_generator.samples // batch_size,
-    epochs=num_epochs,
-    validation_data=validation_generator,
-    validation_steps=validation_generator.samples // batch_size)
+# # Define data generators for training and validation
+# train_datagen = ImageDataGenerator(
+#     rescale=1./255,
+#     shear_range=0.2,
+#     zoom_range=0.2,
+#     horizontal_flip=True)
+
+# test_datagen = ImageDataGenerator(rescale=1./255)
+# #fully connected layer that will iterate over dataset
+
+# train_generator = train_datagen.flow(
+#     data, #training data directory
+#     # target_size=input_shape[:2],
+#     batch_size=batch_size,
+#     class_mode='categorical')
+
+# validation_generator = test_datagen.flow(
+#     y, #validation data directory
+#     target_size=input_shape[:2],
+#     batch_size=batch_size,
+#     class_mode='categorical')
+
+# # Train the model
+# history = model.fit_generator(
+#     train_generator,
+#     steps_per_epoch=train_generator.samples // batch_size,
+#     epochs=num_epochs,
+#     validation_data=validation_generator,
+#     validation_steps=validation_generator.samples // batch_size)
 
 # Save the trained model
 model.save('vgg16_trained.h5')
 
+# Plot the training and validation accuracy
+plt.plot(history.history['accuracy'])
+plt.plot(history.history['val_accuracy'])
+plt.title('Model Accuracy')
+plt.xlabel('Epoch')
+plt.ylabel('Accuracy')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+# Plot the training and validation loss
+plt.plot(history.history['loss'])
+plt.plot(history.history['val_loss'])
+plt.title('Model Loss')
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.legend(['Train', 'Validation'], loc='upper left')
+plt.show()
+
+
 # To show all the data in npy file as images [FOR DEMO PURPOSES ONLY]
-img_array=np.load(r'/mnt/c/Users/John/Documents/Datasets/task7/task7_X_test.npy')
-i = 0
-while i < len(img_array):
-   plt.imshow(img_array[i])
-   plt.show()
-   i += 1
-   img = Image.fromarray(img_array[i], 'RGB')
-   img.show()
-   img.save('sample.png')
-   i += 1
+# img_array=np.load(r'/mnt/c/Users/John/Documents/Datasets/task7/task7_X_test.npy')
+# i = 0
+# while i < len(img_array):
+#    plt.imshow(img_array[i])
+#    plt.show()
+#    i += 1
+#    img = Image.fromarray(img_array[i], 'RGB')
+#    img.show()
+#    img.save('sample.png')
+#    i += 1
 
 """ 
 load the VGG16 model without the fully connected layers using the 
@@ -93,28 +150,3 @@ hen, we add new fully connected layers to the model, compile it with the adam op
 using the ImageDataGenerator function from Keras. 
 Finally, we train the model using the fit_generator function from Keras 
 and save the trained model using the save function."""
-
-# labels = np.load(r'/mnt/c/Users/John/Documents/Datasets/task7/task7_y_test.npy')
-# n_rows = data.shape[0]
-# w = data.shape[1]
-# d = data.shape[2]
-# n_validation = int(n_rows * 0.1)
-# n_train = n_rows - n_validation
-
-# X_train = data[:n_train]
-# y_train = labels[:n_train]
-# X_val = data[-n_validation:]
-# y_val = labels[-n_validation:]
-
-# x_train_bw, x_val_bw = np.mean(X_train, axis=-1), np.mean(X_val, axis=-1)
-# x_train_final = np.reshape(x_train_bw, (n_train, w*d))
-# x_val_final = np.reshape(x_val_bw, (n_validation, w*d))
-
-# data = np.load(r'/mnt/c/Users/John/Documents/Datasets/task7/task7_X_train.npy')
-# labels = np.load(r'/mnt/c/Users/John/Documents/Datasets/task7/task7_y_train.npy')
-# # Categories = os.listdir(labels)
-# # print('Total Number of Categories: ', len(Categories))
-# # print('CategoriesList: ', Categories)
-# print(data.shape)
-# print(labels.shape)
-
