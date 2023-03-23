9ded2e550cc4ee405b093009235881fd5c5179c1
John Lawrence Abarquez <lawrence.abarquez@gmail.com>
2023-03-23 14:00:22 +0800

    Transferred all scripts to one folder

---
+++ scripts/data_as_array.py
@@ -0,0 +1,8 @@
+import numpy as np
+img_array = np.load(r"/mnt/c/Windows/System32/repos/thesis_raw_data/task5/task5_X_train.npy")
+# img_array = np.load(r"/mnt/c/Windows/System32/repos/thesis_raw_data/task5/task5_y_train.npy")
+
+i = 0
+while i < len(img_array):
+    print(img_array[i],i)
+    i += 1
